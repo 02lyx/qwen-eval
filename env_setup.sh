35195f30dd924ada91c6283cc4c255a43d7b5fab
02lyx <yuanxinliu2029@u.northwestern.edu>
2025-03-27 21:16:03 -0500

    init

---
+++ env_setup.sh
@@ -0,0 +1,6 @@
+wget -qO- https://astral.sh/uv/install.sh | sh
+uv venv briter --python 3.11 && source briter/bin/activate && uv pip install --upgrade pip --link-mode=copy
+uv pip install -r requirements.txt --link-mode=copy
+uv pip install flash_attn --no-build-isolation --link-mode=copy
+uv uninstall wandb
+uv pip install wandb --no-cache-dir --link-mode=copy
---
+++ eval_1.sh
@@ -0,0 +1,20 @@
+# Qwen2.5-Math-Instruct Series
+PROMPT_TYPE="qwen-boxed"
+
+export HF_TOKEN=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
+
+# 设置要评估的模型
+MODEL_NAME_OR_PATH="Yuanxin-Liu/Qwen2.5-7B_Mix-Math-yt-rbt-grpo_0.5_exp12_gen_8_test_8_clip_ratio_-1_outer_kl-320"
+OUTPUT_DIR="results-wihout-clip-320-sampling"
+
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+
+echo "========================================================"
+echo "开始评估基础模型: $MODEL_NAME_OR_PATH"
+echo "输出目录: $OUTPUT_DIR"
+echo "========================================================"
+
+# 执行评估
+bash sh/eval.sh $PROMPT_TYPE $MODEL_NAME_OR_PATH $OUTPUT_DIR
+
+echo "基础模型评估完成！"
