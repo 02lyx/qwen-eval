57cb51357567a16f3fea93cef27de8436637e6c6
02lyx <yuanxinliu2029@u.northwestern.edu>
2025-03-27 21:20:45 -0500

    change cuda_visible

--- eval_2.sh
+++ eval_2.sh
@@ -7,7 +7,7 @@ export HF_TOKEN=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
 MODEL_NAME_OR_PATH="Yuanxin-Liu/Qwen2.5-7B_Mix-Math-yt-rbt-grpo_0_exp0_gen_8_test_8_clip_ratio_0_outer_kl-320"
 OUTPUT_DIR="results-baseline-320-sampling"
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=4,5,6,7
 
 echo "========================================================"
 echo "开始评估基础模型: $MODEL_NAME_OR_PATH"
